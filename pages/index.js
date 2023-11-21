e7117f3f27296d8679d1285fc346d91a49e1694d
Georgi Topalov <g.s.topalov@gmail.com>
2023-11-21 14:17:13 +0700

    fixed imports for css

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Layout from '../components/layout';
 
 const HomePage = () => {
   return (
+    <Layout>
     <div>
       <h1>Welcome to the store!</h1>
     </div>
+    </Layout>
   );
 };
 
