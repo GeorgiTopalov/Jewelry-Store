03bc791dd2ded170b114dde4aa2602d24ddedf5c
Georgi Topalov <g.s.topalov@gmail.com>
2023-11-23 14:20:54 +0700

    Added option to add extra class

--- components/collabsibleSection.js
+++ components/collabsibleSection.js
@@ -1,16 +1,18 @@
-// components/CollapsibleSection.js
 import React, { useState } from 'react';
 import Link from 'next/link';
 
-const CollapsibleSection = ({ title, links }) => {
+const CollapsibleSection = ({ title, links, additionalClass }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleOpen = () => setIsOpen(!isOpen);
 
+  // Generate the class name
+  const className = `${title.toLowerCase().replace(' ', '-')} ${additionalClass || ''}`;
+
   return (
-    <div className={title.toLowerCase().replace(' ', '-')}>
+    <div className={className}>
       <h3 onClick={toggleOpen}>{title}</h3>
-      <ul style={{ maxHeight: isOpen ? '500px' : '0', transition: 'max-height 0.3s ease-out', overflow: 'hidden' }}>
+      <ul style={{ maxHeight: isOpen ? '600px' : '0', transition: 'max-height 0.3s ease-out', overflow: 'hidden' }}>
         {links.map((link, index) => (
           <li key={index}>
             <Link href={link.href}>{link.text}</Link>
