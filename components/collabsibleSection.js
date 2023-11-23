ba3f96fff59b6620e0007e7b4f5b2ddc66c59905
Georgi Topalov <g.s.topalov@gmail.com>
2023-11-23 15:53:11 +0700

    Added desktop styling for the footer

--- components/collabsibleSection.js
+++ components/collabsibleSection.js
@@ -1,37 +1,43 @@
-import React, { useState, useEffect } from 'react';
-import Link from 'next/link';
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
 
 const CollapsibleSection = ({ title, links, additionalClass }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
-    
     const checkWidth = () => {
       const screenWidth = window.innerWidth;
-      setIsOpen(screenWidth >= 768); 
+      setIsOpen(screenWidth >= 768);
     };
 
     checkWidth();
-    window.addEventListener('resize', checkWidth);
+    window.addEventListener("resize", checkWidth);
 
     // Cleanup
-    return () => window.removeEventListener('resize', checkWidth);
+    return () => window.removeEventListener("resize", checkWidth);
   }, []);
 
   const toggleOpen = () => {
     const screenWidth = window.innerWidth;
-    if (screenWidth < 800) { 
+    if (screenWidth < 800) {
       setIsOpen(!isOpen);
     }
   };
 
-  const className = `${title.toLowerCase().replace(' ', '-')} ${additionalClass || ''}`;
-
+  const className = `${title.toLowerCase().replace(" ", "-")} ${
+    additionalClass || ""
+  }`;
 
   return (
     <div className={className}>
       <h3 onClick={toggleOpen}>{title}</h3>
-      <ul style={{ maxHeight: isOpen ? '500px' : '0', transition: 'max-height 0.3s ease-out', overflow: 'hidden' }}>
+      <ul
+        style={{
+          maxHeight: isOpen ? "500px" : "0",
+          transition: "max-height 0.3s ease-out",
+          overflow: "hidden",
+        }}
+      >
         {links.map((link, index) => (
           <li key={index}>
             <Link href={link.href}>{link.text}</Link>
