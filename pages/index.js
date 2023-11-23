00e6497adabc57f924e08009790964224745e2e7
Georgi Topalov <g.s.topalov@gmail.com>
2023-11-23 11:26:02 +0700

    Hero and Newsletter styling

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import Layout from '../components/layout';
+import React from "react";
+import Layout from "../components/layout";
 
 const HomePage = () => {
   return (
     <Layout>
-    <div>
-      <h1>Welcome to the store!</h1>
-    </div>
+      <div className="hero-img">
+        <div className="hero-content">
+          <h1>Luxury Within Reach</h1>
+          <div className="hero-content-bottom">
+            <h3>Experience the Brilliance of Lab-Grown Diamonds</h3>
+            <button className="btn btn-primary">Shop Now</button>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
