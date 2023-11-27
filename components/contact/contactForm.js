d928205ec47fdd3c2da673fa51e16c6868345c0c
Georgi Topalov <g.s.topalov@gmail.com>
2023-11-27 08:58:07 +0700

    Added more components and modules

--- components/contact/contactForm.js
+++ components/contact/contactForm.js
@@ -1,17 +1,18 @@
 import React from "react";
 import Link from "next/link";
+import styles from "../../styles/contact.module.css";
 
 
 
 const ContactForm = () => {
     return (
-        <div className="contact-card">
-        <div className="contact-text">
+        <div className={styles['contact-card']}>
+        <div className={styles['contact-text']}>
             <h2>Contact Us</h2>
             <p>Don't forget to check our <Link href="/faq">FAQ</Link> page for frequently asked question.
              You may find what you are looking to ask there quicker.</p>
         </div>
-        <form id="contact-form">
+        <form id={styles['contact-form']}>
             <div className="input-col">
             <label>Your Name</label>
             <input className="contact-input"></input>
