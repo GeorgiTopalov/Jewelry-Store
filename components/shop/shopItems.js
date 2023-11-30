21dbecd1bcd43161dd5b8fd1b09bce4e011f38af
Georgi Topalov <g.s.topalov@gmail.com>
2023-11-30 12:41:15 +0700

    Added more styling to the shop area

--- components/shop/shopItems.js
+++ components/shop/shopItems.js
@@ -6,36 +6,44 @@ const ShopItems = () => {
   return (
     <div id={styles["items-area"]}>
       <div className={styles['product']}>
+      <Link href="/productdetail">
       <div className={styles['product-card']}>
         <Link href="/productdetails" className={`btn ${styles['btn-shop']}`}>
           <h3>Add to Cart</h3>
         </Link>
       </div>
+      </Link>
       <p>This is the rings name</p>
       <p className={styles['price']}>900$</p>
       </div>
       <div className={styles['product']}>
+      <Link href="/productdetail">
       <div className={styles['product-card']}>
         <Link href="/productdetails" className={`btn ${styles['btn-shop']}`}>
           <h3>Add to Cart</h3>
         </Link>
       </div>
+      </Link>
       <p>This is the rings name</p>
       <p className={styles['price']}>900$</p>
       </div><div className={styles['product']}>
+      <Link href="/productdetail">
       <div className={styles['product-card']}>
         <Link href="/productdetails" className={`btn ${styles['btn-shop']}`}>
           <h3>Add to Cart</h3>
         </Link>
       </div>
+      </Link>
       <p>This is the rings name</p>
       <p className={styles['price']}>900$</p>
       </div><div className={styles['product']}>
+      <Link href="/productdetail">
       <div className={styles['product-card']}>
         <Link href="/productdetails" className={`btn ${styles['btn-shop']}`}>
           <h3>Add to Cart</h3>
         </Link>
       </div>
+      </Link>
       <p>This is the rings name</p>
       <p className={styles['price']}>900$</p>
       </div>
