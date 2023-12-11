74f72623850bc3ebb4094ddd97ea9bcaa2f6d343
Georgi Topalov <g.s.topalov@gmail.com>
2023-12-11 08:28:19 +0700

    added account page and components

---
+++ components/account/accountComponent.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Link from "next/link";
+import styles from "./../../styles/account/account.module.css";
+
+
+const BlogLayout = () => {
+    return (
+        <div></div>
+    );
+};
---
+++ pages/account.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/layout.jsx";
+
+
+
+const AccountPage = () => {
+    return (
+      <Layout>
+       <AccountLayout/>
+      </Layout>
+    );
+  };
+  
+  export default AccountPage;
