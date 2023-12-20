6a2a2fd3ef637dcaeeebf201ec092a70168bacf3
Georgi Topalov <g.s.topalov@gmail.com>
2023-12-20 15:13:40 +0700

    Added delivery section in productdetails

--- components/shop/product.jsx
+++ components/shop/product.jsx
@@ -61,10 +61,27 @@ const ProductComponent = () => {
                 <option>8</option>
               </select>
             </div>
-            <button className="btn btn-shop form-section" type="submit">
+            <button
+              className={`btn ${styles["btn-shop"]} ${styles["form-section"]}`}
+              type="submit"
+            >
               1000$<span className={styles["add-to-bag"]}>Add to Bag</span>
             </button>
           </form>
+          
+          <div className={styles["delivery"]}>
+            <div className={styles["delivery-svg"]}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="32"
+                width="40"
+                viewBox="0 0 640 512"
+              >
+                <path d="M48 0C21.5 0 0 21.5 0 48V368c0 26.5 21.5 48 48 48H64c0 53 43 96 96 96s96-43 96-96H384c0 53 43 96 96 96s96-43 96-96h32c17.7 0 32-14.3 32-32s-14.3-32-32-32V288 256 237.3c0-17-6.7-33.3-18.7-45.3L512 114.7c-12-12-28.3-18.7-45.3-18.7H416V48c0-26.5-21.5-48-48-48H48zM416 160h50.7L544 237.3V256H416V160zM112 416a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm368-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z" />
+              </svg>
+            </div>
+            <p className={styles["delivery-text"]}>Free shipping. Order now and your order will be shipped within 10 days!</p>
+          </div>
           <div className={styles["info-section"]}>
             <h3>Description and details</h3>
             <p>
