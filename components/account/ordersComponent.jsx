118d5191167be548735c370ae1051f451159a341
Georgi Topalov <g.s.topalov@gmail.com>
2023-12-27 09:13:46 +0700

    added a table to the acc-orders

--- components/account/ordersComponent.jsx
+++ components/account/ordersComponent.jsx
@@ -3,11 +3,47 @@ import Link from "next/link";
 import styles from "./../../styles/account/orders.module.css";
 import AccountLayout from "./accountLayoutComponent";
 
+const Orders = [
+  {Details: "Gold ring 1ct diamond", Date: "12/10/2023", Status: "shipped", Tracking: "923293"},
+  {Details: "White Gold earrings 1ct diamond", Date: "11/7/2021", Status: "complete", Tracking: "123093"},
+]
+
 const OrdersComponent = () => {
+  if (Orders.length === 0) {
+    return (
+      <AccountLayout>
+        <div className={styles['orders-content']}>
+          <h4>My Orders</h4>
+          <p className={styles["orders-empty"]}>No orders yet</p>
+        </div>
+      </AccountLayout>
+    );
+  }
+
+  const headers = Object.keys(Orders[0]);
+
   return (
     <AccountLayout>
-      <div className={styles["orders-content"]}>
+      <div className={styles['orders-content']}>
         <h4>My Orders</h4>
+        <table>
+          <thead>
+            <tr>
+              {headers.map(header => (
+                <th key={header}>{header}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {Orders.map((order, index) => (
+              <tr key={index} >
+                {headers.map(header => (
+                  <td key={header} className={styles["table-data"]}>{order[header]}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </AccountLayout>
   );
