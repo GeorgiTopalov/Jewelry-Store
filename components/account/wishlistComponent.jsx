c3824851ca60e5b60dcf516c644338ccf2a1deb3
Georgi Topalov <g.s.topalov@gmail.com>
2023-12-28 09:01:45 +0700

    added styling to the acc wishlist

--- components/account/wishlistComponent.jsx
+++ components/account/wishlistComponent.jsx
@@ -1,9 +1,48 @@
-import React from "react";
-import Link from "next/link";
-import styles from "./../../styles/account/wishlist.module.css";
-import AccountLayout from "./accountLayoutComponent";
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import styles from './../../styles/account/wishlist.module.css';
+import AccountLayout from './accountLayoutComponent';
 
-const wishProducts = [];
+// const WishlistComponent = () => {
+//   const [wishProducts, setWishProducts] = useState([]);
+
+//   useEffect(() => {
+//     const fetchProducts = async () => {
+//       // Replace this URL with your actual endpoint
+//       const response = await fetch('https://your-database-endpoint.com/products');
+//       const data = await response.json();
+//       setWishProducts(data);
+//     };
+
+//     fetchProducts();
+//   }, []);
+
+const wishProducts = [
+  // {
+  //   id: "274",
+  //   imageUrl: "/Ring-sample1.jpg",
+  //   price: "1000",
+  //   name: "Super awesome ring with beautiful diamond",
+  // },
+  // {
+  //   id: "274",
+  //   imageUrl: "/Ring-sample1.jpg",
+  //   price: "1000",
+  //   name: "Super awesome ring with beautiful diamond",
+  // },
+  // {
+  //   id: "274",
+  //   imageUrl: "/Ring-sample1.jpg",
+  //   price: "1000",
+  //   name: "Super awesome ring with beautiful diamond",
+  // },
+  // {
+  //   id: "274",
+  //   imageUrl: "/Ring-sample1.jpg",
+  //   price: "1000",
+  //   name: "Super awesome ring with beautiful diamond",
+  // },
+];
 
 const WishlistComponent = () => {
   if (wishProducts.length === 0) {
@@ -23,23 +62,20 @@ const WishlistComponent = () => {
     <AccountLayout>
       <div className={styles["wishlist-content"]}>
         <h4>Wishlist</h4>
-        <div>
-          <ul>
-            <li>
-              <div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="16"
-                  width="16"
-                  viewBox="0 0 512 512"
-                >
-                  <path d="M119.4 44.1c23.3-3.9 46.8-1.9 68.6 5.3l49.8 77.5-75.4 75.4c-1.5 1.5-2.4 3.6-2.3 5.8s1 4.2 2.6 5.7l112 104c2.9 2.7 7.4 2.9 10.5 .3s3.8-7 1.7-10.4l-60.4-98.1 90.7-75.6c2.6-2.1 3.5-5.7 2.4-8.8L296.8 61.8c28.5-16.7 62.4-23.2 95.7-17.6C461.5 55.6 512 115.2 512 185.1v5.8c0 41.5-17.2 81.2-47.6 109.5L283.7 469.1c-7.5 7-17.4 10.9-27.7 10.9s-20.2-3.9-27.7-10.9L47.6 300.4C17.2 272.1 0 232.4 0 190.9v-5.8c0-69.9 50.5-129.5 119.4-141z" />
-                </svg>
-                <button>Add to bag</button>
-              </div>
-            </li>
+          <ul className={styles["wishlist-items"]}>
+              {wishProducts.map((product) => (
+                <li key={product.id}>
+                  <p>{product.name}</p>
+                  <div
+                    className={styles["wish-img"]}
+                    style={{ backgroundImage: `url(${product.imageUrl})` }}
+                  >
+                  </div>
+                  <button className="btn">Add to bag</button>
+                  <p className={styles["wish-price"]}><span>Price:</span>{product.price}$</p>
+                </li>
+              ))}
           </ul>
-        </div>
       </div>
     </AccountLayout>
   );
