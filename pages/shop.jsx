c4a044e5332153c0b752e7278ea81aef79a243bd
Georgi Topalov <g.s.topalov@gmail.com>
2024-02-29 15:23:47 +0700

    restructured the navbar

--- pages/shop.jsx
+++ pages/shop.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import Layout from "../components/layout";
+import Layout from "../components/layout.jsx";
 import ShopCategories from "./../components/shop/shopCategories";
-import FilterSection from "./../components/shop/filter";
-import ShopItems from "./../components/shop/shopItems";
-import styles from "./../styles/shop/shopArea.module.css";
+import FilterSection from "./../components/shop/filter.jsx";
+import ShopItems from "./../components/shop/shopItems.jsx";
+import styles from "./../components/shop/shopArea.module.css";
 
 const HomePage = () => {
   return (
