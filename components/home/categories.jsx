8e30f3c1e9a0befb8161599c271ffd24c22ed977
Georgi Topalov <g.s.topalov@gmail.com>
2024-03-05 11:25:30 +0700

    updated some of the code in categories for the home page

--- components/home/categories.jsx
+++ components/home/categories.jsx
@@ -1,21 +1,40 @@
 import React from "react";
 import styles from "./categories.module.css";
+import Link from "next/link";
+
+const categories = [
+  { href: "/shop", label: "Rings", className: styles["rings"] },
+  { href: "/shop", label: "Earrings", className: styles["earrings"] },
+  { href: "/shop", label: "Bracelets", className: styles["bracelets"] },
+];
+
+
 
 const HomeCategoriesComponent = () => {
-  return (
-    <div className={styles["categories"]}>
-      <h2 className={styles["title"]}>Explore Our Selections</h2>
-      <div id={styles["grid"]}>
-        <div className={styles["rings"]}></div>
-        <div className={styles["earrings"]}></div>
-        <div className={styles["bracelets"]}></div>
+  return (  
+      <div className={styles["categories"]}>
+        <h2 className={styles["title"]}>Explore Our Selections</h2>
+        <div id={styles["grid"]}>
+          {categories.map((category) => (
+            <Link
+              key={category.label}
+              href={category.href}
+              className={category.className}
+            ></Link>
+          ))}
+        </div>
+        <div className={styles["category-names"]}>
+          {categories.map((category) => (
+            <Link
+              key={category.label}
+              href={category.href}
+              className={styles["category"]}
+            >
+              <h3>{category.label}</h3>
+            </Link>
+          ))}
+        </div>
       </div>
-      <div className={styles["category-names"]}>
-        <h3>Rings</h3>
-        <h3>Earrings</h3>
-        <h3>Bracelets</h3>
-      </div>
-    </div>
   );
 };
 
