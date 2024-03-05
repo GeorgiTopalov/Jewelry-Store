25117a613bff03b3f4e7b8d866ec7833b008db5f
Georgi Topalov <g.s.topalov@gmail.com>
2024-03-05 14:02:14 +0700

    stylized the landing page

--- components/home/mostPopular.jsx
+++ components/home/mostPopular.jsx
@@ -2,10 +2,38 @@ import React from "react";
 import styles from "./mostPopular.module.css";
 import Link from "next/link";
 
+const mostPopularItems = [
+  {
+    href: "/shop/productdetail",
+    className: styles["card"],
+    background: "url('/images/Ring-sample1.jpg')",
+  },
+  {
+    href: "/shop/productdetail",
+    className: styles["card"],
+    background: "url('/images/Ring-sample2.jpg')",
+  },
+  {
+    href: "/shop/productdetail",
+    className: styles["card"],
+    background: "url('/images/Ring-sample3.jpg')",
+  },
+];
+
 const MostPopularComponent = () => {
   return (
     <div className={styles["most-popular"]}>
       <h2 className={styles["title"]}>Most Popular Jewelry</h2>
+      <div className={styles["most-popular-items"]}>
+        {mostPopularItems.map((item) => (
+          <Link
+            key={item.href}
+            href={item.href}
+            className={item.className}
+            background={item.background}
+          ></Link>
+        ))}
+      </div>
       <Link href="/shop" className={styles["shop-link"]}>
         <h4>Shop Now</h4>
       </Link>
